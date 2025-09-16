23185b911cc9c489a70bd145ee5de2085224ec25
Rishit Shetty <rishitshetty99@gmail.com>
2025-09-16 18:17:03 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,13 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/Portfolio/",
   server: {
     host: "::",
     port: 8080,
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  plugins: [react()], // Remove the componentTagger reference
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
